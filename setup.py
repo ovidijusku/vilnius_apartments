8a60bde625f7e35a05d015dd9dc1998403f37469
Ovidijus <70970616+ovidijusku@users.noreply.github.com>
2021-02-04 19:17:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package of web scrapper that extracts apartments data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ovidijusku/vilnius_apartments/tree/main/module",
+    url="https://github.com/ovidijusku/vilnius_apartments/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
